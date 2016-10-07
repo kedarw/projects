2c9b92920d83dd0affbc45f20fb19b53c9b781f4
kedarw <kedarwaghmode@gmail.com>
2016-10-06 21:40:51 -0700

    Added list of histograms that need to ignored

--- vscsi_stats/parse_vscsi_stats.py
+++ vscsi_stats/parse_vscsi_stats.py
@@ -7,19 +7,29 @@ from collections import defaultdict
 from collections import Counter
 import glob
 
+ignore_list = ["Histogram: IO lengths of Write commands",
+               "Histogram: IO lengths of Read commands",
+               "Histogram: distance (in LBNs) between successive Write commands",
+               "Histogram: distance (in LBNs) between successive Read commands",
+               "Histogram: distance (in LBNs) between each command from the closest of previous 16",
+               "Histogram: latency of IOs in Microseconds (us)",
+               "Histogram: number of outstanding IOs when a new IO is issued",
+               "Histogram: latency of IO interarrival time in Microseconds (us)"
+               ]
+
 def parse_stat_files(stats_dir):
     print ("Parsing stat files in directory : "+str(stats_dir))
 
-    old_files = glob.glob(str(stats_dir)+"/stats*_o")
+    old_files = glob.glob(str(stats_dir)+"/stats*.2")
     for old_file in old_files:
         os.remove(old_file)
 
     stat_files = glob.glob(str(stats_dir)+"/stats*")
+
     no_of_files = 0
     exclude_keywords = ['Histogram', 'min', 'max', 'mean', 'count']
 
     for stat_file in stat_files:
-
         histos_data = defaultdict(list)
 
         with open(stat_file) as origin_file:
@@ -36,12 +46,13 @@ def parse_stat_files(stats_dir):
                     key_value = temp.split(",")
                     histos_data[histos_heading].append((int(key_value[1]), int(key_value[0])))
 
-        unique_histos = set(histos)
+        unique_histos = set(histos) - set(ignore_list)
 
-        f = file(str(stat_file)+"_o", "w")
+        f = file(str(stat_file)+".2", "w")
 
         for histo in unique_histos:
             f.write(str(histo)+'\n')
+            f.write('min,0\nmax,0\nmean,0\ncount,0\nFrequency,Histogram Bucket Limit\n')
 
             data_lists = []
             for data in histos_data:
